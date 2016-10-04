aa295668665db9a028956e8ed7106d9f8a247d1c
劉蒨蓉 <eeit8819@III-ITC>
2016-10-04 13:06:30 +0800

    第三次commit
    修改AddServlet，新增一個PrintWriter，並在畫面上印出。

--- LabWebServletJdbcSpringMaven01/src/main/java/add/AddServlet.java
+++ LabWebServletJdbcSpringMaven01/src/main/java/add/AddServlet.java
@@ -1,6 +1,7 @@
 package add;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,10 @@ public class AddServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<h3>This is third commit add PrintWriter</h3>");
 		System.out.println("AAA");
+		
 	}
 
 	@Override
